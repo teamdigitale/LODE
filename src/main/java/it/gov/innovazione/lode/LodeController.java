6d58a5364edf986c4bb5680d5eb35efe9751a69d
Giuseppe Nespolino <gnespolino@users.noreply.github.com>
2022-12-05 12:39:43 +0100

    config (#8)

--- src/main/java/it/gov/innovazione/lode/LodeController.java
+++ src/main/java/it/gov/innovazione/lode/LodeController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,7 +12,6 @@ import java.util.Collection;
 import java.util.List;
 
 @Controller
-@RequestMapping("lode")
 @RequiredArgsConstructor
 public class LodeController {
 
